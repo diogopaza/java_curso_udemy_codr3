e64e8b21224d458f316ed0a41daa374d9934fd7c
diogo paza <diogopazacvel@gmail.com>
2020-10-25 18:38:34 -0300

    final modulo 5

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -4,10 +4,12 @@ public class Produto {
 
 	String nome;
 	double preco;
-	double desconto;
+	double precoComDesconto;
+	static double desconto = 0.25;
 
-	Produto(String nomeInicial) {
+	Produto(String nomeInicial,double preco) {
 		this.nome = nomeInicial;
+		this.precoComDesconto = preco * (1 - desconto);   
 	}
 
 	Produto() {
--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -4,26 +4,12 @@ public class ProdutoTeste {
 	
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto("Notebook");
-		//p1.nome = "Notebook";
-		p1.preco = 4359.89;
-		p1.desconto = 0.25;
-		var p2 = new Produto("Caneta preta");
-		//p2.nome = ;
-		p2.preco = 12;
-		p2.desconto = 0.29;
+		Produto p1 = new Produto("Notebook", 4359.89);
+		var p2 = new Produto("Caneta preta", 10);
 		
-		System.out.println(p1.nome);
-		System.out.println(p2.nome);
-		
-		/*
-		double precoFinal1 = p1.preco * (1 - p1.desconto);
-		double precoFinal2 = p2.preco * (1 - p2.desconto);
-		*/
-		double precoFinal1 = p1.precoComDesconto();
-		
-		System.out.printf("Valor final do %s é de %.2f ", p1.nome,precoFinal1);
-		
+				
+		System.out.printf("Valor final do %s é de %.2f ", p2.nome, p2.precoComDesconto);
+		System.out.printf("Valor final do %s é de %.2f ", p1.nome, p1.precoComDesconto);
 		
 		
 		
